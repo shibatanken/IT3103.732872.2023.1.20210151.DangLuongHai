6368fb1678827e87c90c5c4b5ec9b9eede13a98d
HaiDang <115143960+shibatanken@users.noreply.github.com>
2023-11-15 23:42:45 +0700

    Add files via upload

---
+++ Lab_02/Cart.java
@@ -0,0 +1,102 @@
+package LAB_02;
+
+public class Cart {
+	public static final int MAX_NUMBER_ORDERED = 20;
+	private DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+	private int qtyOrdered=0;
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(qtyOrdered<MAX_NUMBER_ORDERED) { 
+			qtyOrdered++;
+			itemsOrdered[qtyOrdered]=disc;
+			System.out.println("Luong Hai Dang - 20210151: Da them san pham vao gio hang !");
+
+		}
+		else { 
+			System.out.println("Luong Hai Dang - 20210151: Gio hang cua ban da day !");
+		}
+	}
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		for(int i=1;i<=qtyOrdered;i++) {
+			if(itemsOrdered[i].equals(disc)) { 
+				for(int j=i;j<=qtyOrdered-1;j++) {
+					itemsOrdered[j]=itemsOrdered[j+1];
+				}
+				qtyOrdered--;
+				System.out.println("Luong Hai Dang - 20210151: Da xoa san pham !");
+				return;
+			}
+		}
+				System.out.println("Luong Hai Dang - 20210151: Khong tim thay san pham !");
+				return;
+	}
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc[] listDvd) {
+		if(qtyOrdered<MAX_NUMBER_ORDERED) { // co the them item
+			qtyOrdered++;
+			for(int i=0;i<listDvd.length;i++) {
+				itemsOrdered[qtyOrdered]= listDvd[i];	
+			}
+			System.out.println("Luong Hai Dang - 20210151: Da them san pham vao gio hang !");
+
+		}
+		else { // gio hang da full
+			System.out.println("Luong Hai Dang - 20210151: Gio hang cua ban da day !");
+		}
+	}
+	
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2) {
+		if(qtyOrdered<MAX_NUMBER_ORDERED-1) { // co the them item
+			qtyOrdered +=2 ;
+			itemsOrdered[qtyOrdered]=dvd1;
+			itemsOrdered[qtyOrdered]=dvd2;
+			System.out.println("Luong Hai Dang - 20210151: Da them san pham vao gio hang !");
+
+		}
+		else { // gio hang da full
+			System.out.println("Luong Hai Dang - 20210151: Gio hang cua ban da day !");
+		}
+	}
+	
+	public float totalCost() {
+		float sum=0;
+		for(int i=1;i<=qtyOrdered;i++) {
+			sum+=itemsOrdered[i].getCost();
+		}
+		return sum;
+	}
+	
+	// Luong Hai Dang - 20210151
+	
+	public void searchDVDById(int searchId) {
+		for(int i=1;i<=qtyOrdered;i++) {
+	        if (itemsOrdered[i].getId() == searchId) {
+	            System.out.println("Tim thay  " + itemsOrdered[i]);
+	            return;
+	        }
+	    }
+	    System.out.println("Khong tim thay DVD co ID: " + searchId);
+	}
+	public void searchDVDByTitle(String searchTitle) {
+		  for(int i=1;i<=qtyOrdered;i++) {
+	        if (itemsOrdered[i].getTitle()==searchTitle) {
+	            System.out.println("Tim thay  " + itemsOrdered[i]);
+	            return;
+	        }
+	    }
+	    System.out.println("Khong tim thay DVD co Title: " + searchTitle);
+	}
+	
+	// Hoang Hai Phong - 20210151
+	
+	public void print() {
+		float totalcost=0;
+		for(int i=1;i<=qtyOrdered;i++) {
+			System.out.println(itemsOrdered[i]);
+			totalcost+=itemsOrdered[i].getCost();
+			}
+		System.out.println("Total cost:" + totalcost +"$");	
+	
+	}
+}
+
